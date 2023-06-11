4ca116b85211bbd62332e37404974117208d6f6c
Fayaz K <code@fayazk.com>
2023-06-11 11:06:24 +0500

    Init laravel, React, Ant Design and Inertia

---
+++ resources/js/Pages/Admin/Dashboard.jsx
@@ -0,0 +1,9 @@
+import AdminLayout from "@/Layouts/AdminLayout.jsx";
+
+export default function Dashboard() {
+    return (
+        <AdminLayout>
+            <h1>Saas Project</h1>
+        </AdminLayout>
+    );
+}
